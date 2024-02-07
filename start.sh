63332e4f624fcf153be4879a7659f6a94f9765eb
Mahin321 <112240772+Mahin321@users.noreply.github.com>
2024-02-07 06:21:45 +0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,22 @@
-if [ -z $UPSTREAM_REPO ]
-then
+#!/bin/bash
+
+# Create a virtual environment
+python3 -m venv venv
+
+# Activate the virtual environment
+source venv/bin/activate
+
+# Clone repository and install dependencies
+if [ -z "$UPSTREAM_REPO" ]; then
   echo "Cloning main Repository"
   git clone https://github.com/lazyindu/test1.git /LazyPrincess
 else
-  echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /LazyPrincess
+  echo "Cloning Custom Repo from $UPSTREAM_REPO"
+  git clone "$UPSTREAM_REPO" /LazyPrincess
 fi
-cd /LazyPrincess
-pip3 install -U -r requirements.txt
-echo "Starting Bot...."
+
+cd /LazyPrincess || exit 1
+pip install -U -r requirements.txt || exit 1
+
+echo "Starting Bot..."
 python3 bot.py
